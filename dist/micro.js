1e86e6b8dad09dce52ab455b80add579d4d880c8
Ian Hansson <ian.hansson@gmail.com>
2019-11-30 06:10:12 +0000

    setup microlist type

--- dist/micro.js
+++ dist/micro.js
@@ -1,1 +1,30 @@
-console.log('hello 2');
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var MicroList = /** @class */ (function (_super) {
+    __extends(MicroList, _super);
+    function MicroList(items) {
+        var _this = _super.apply(this, items) || this;
+        Object.setPrototypeOf(_this, Object.create(MicroList.prototype));
+        return _this;
+    }
+    MicroList.prototype.check = function () {
+        console.log('hello');
+        return this;
+    };
+    return MicroList;
+}(Array));
+function Find(selector) {
+    var els = document.querySelectorAll(selector);
+    return new MicroList(Array.prototype.slice.call(els));
+}
--- src/micro.ts
+++ src/micro.ts
@@ -1,1 +1,15 @@
-console.log('hello 2')
+class MicroList extends Array<Element> {
+    constructor(items?: Array<Element>) {
+        super(...items);
+        (<any>Object).setPrototypeOf(this, Object.create(MicroList.prototype));
+    }
+	check(): MicroList {
+		console.log('hello')
+		return this;
+	}
+}
+
+function Find(selector: string): MicroList {
+	const els: NodeList = document.querySelectorAll(selector);
+	return new MicroList(Array.prototype.slice.call(els));
+}
